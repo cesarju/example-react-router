81795629b5c4c2d6df8898b3797ba223f625d708
Cesar Ticona <cesarju.cc@gmail.com>
2023-06-02 18:26:00 -0400

    description react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import ListDogs from "./pages/ListDogs";
 
+//npm i react-router-dom
+//Realizar los enrutamientos con la libreria de react-router-dom
 function App() {
   return (
     <div className="App">
-      {/* Realizar los enrutamientos con la libreria de react-router-dom */}
       <ListDogs />
     </div>
   );
